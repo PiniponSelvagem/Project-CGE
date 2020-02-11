003cdcf9b70b57f85ebe684e8f04af69b177eff7
PiniponSelvagem <piniponselvagem@gmail.com>
2020-02-11 16:41:06 +0000

    Fixed incorrect camera pitch.

--- src/engine/Camera.h
+++ src/engine/Camera.h
@@ -21,7 +21,7 @@ class Camera {
 
 
 		void updateCameraVectors() {
-			front.x = cos(glm::radians(yaw) * cos(glm::radians(pitch)));
+			front.x = cos(glm::radians(yaw)) * cos(glm::radians(pitch));
 			front.y = sin(glm::radians(pitch));
 			front.z = sin(glm::radians(yaw) * cos(glm::radians(pitch)));
 
